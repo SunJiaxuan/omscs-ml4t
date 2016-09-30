78806c6c8b02c25b8e7a8ee4f5a1009c214a1e2b
Anuj Mehta <anuj.nm@gmail.com>
2016-09-29 21:54:10 -0700

    First commit, with mc1_p1 and mc1_p2.

--- mc1_p2/optimization.py
+++ mc1_p2/optimization.py
@@ -1,37 +1,76 @@
-"""MC1-P2: Optimize a portfolio."""
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 import datetime as dt
 from util import get_data, plot_data
+import scipy.optimize as spo
+#from analysis import get_portfolio_value, get_portfolio_stats
+
+
+def get_portfolio_stats(port_val, daily_rf=0, samples_per_year=252):
+    daily_returns = (port_val - port_val.shift(1)) / port_val.shift(1)
+    daily_returns = daily_returns[1:]
+    cr = float(port_val[-1])/port_val[0] - 1.0
+    adr = daily_returns.mean()
+    sddr = daily_returns.std()
+    sr = samples_per_year / np.sqrt(samples_per_year) * (adr - daily_rf) / sddr
+
+    return cr, adr, sddr, sr
+
+
+def error_func(allocs, prices):
+    returns = prices / prices.ix[0]
+    port_val = (returns * allocs).sum(axis=1)
+    _, _, _, sr = get_portfolio_stats(port_val)
+    
+    return -sr
+    
+    
+def find_optimal_allocations(prices):
+    # Initializing to equal values
+    init_guess = np.ones(prices.shape[1], dtype=np.float64) * 1.0 / prices.shape[1]
+    alloc_bounds = [(0, prices.shape[1])] * prices.shape[1]
+    alloc_constraint = ({'type': 'eq', 'fun': lambda x: 1 - np.sum(x)})
+    min_result = spo.minimize(error_func,
+                              init_guess,
+                              args=(prices, ),
+                              method='SLSQP',
+                              options={'disp:': True},
+                              bounds=alloc_bounds,
+                              constraints=alloc_constraint)
+    return min_result.x
+
 
 # This is the function that will be tested by the autograder
 # The student must update this code to properly implement the functionality
-def optimize_portfolio(sd=dt.datetime(2008,1,1), ed=dt.datetime(2009,1,1), \
-    syms=['GOOG','AAPL','GLD','XOM'], gen_plot=False):
-
+def optimize_portfolio(sd=dt.datetime(2008, 1, 1),
+                       ed=dt.datetime(2009, 1, 1),
+                       syms=['GOOG', 'AAPL', 'GLD', 'XOM'],
+                       gen_plot=False):
     # Read in adjusted closing prices for given symbols, date range
     dates = pd.date_range(sd, ed)
     prices_all = get_data(syms, dates)  # automatically adds SPY
     prices = prices_all[syms]  # only portfolio symbols
     prices_SPY = prices_all['SPY']  # only SPY, for comparison later
 
-    # find the allocations for the optimal portfolio
-    # note that the values here ARE NOT meant to be correct for a test case
-    allocs = np.asarray([0.2, 0.2, 0.3, 0.3, 0.0]) # add code here to find the allocations
-    cr, adr, sddr, sr = [0.25, 0.001, 0.0005, 2.1] # add code here to compute stats
+    # Get optimal allocations
+    allocs = find_optimal_allocations(prices)
+    allocs = allocs / np.sum(allocs)
 
-    # Get daily portfolio value
-    port_val = prices_SPY # add code here to compute daily portfolio values
+    returns = prices / prices.ix[0]
+    port_val = (returns * allocs).sum(axis=1)
+    cr, adr, sddr, sr = get_portfolio_stats(port_val)
 
-    # Compare daily portfolio value with SPY using a normalized plot
+    # Normalize SPY values, and plot against optimal portfolio
     if gen_plot:
-        # add code to plot here
-        df_temp = pd.concat([port_val, prices_SPY], keys=['Portfolio', 'SPY'], axis=1)
-        pass
+        normalized_SPY = prices_SPY / prices_SPY.ix[0, :]
+        chart_data = pd.concat(
+            [port_val, normalized_SPY], keys=['Portfolio', 'SPY'], axis=1)
+
+        plot_data(chart_data, title="Optimal Portfolio vs SPY (Normalized)")
 
     return allocs, cr, adr, sddr, sr
+
 
 def test_code():
     # This function WILL NOT be called by the auto grader
@@ -42,14 +81,15 @@ def test_code():
     # Note that ALL of these values will be set to different values by
     # the autograder!
 
-    start_date = dt.datetime(2009,1,1)
-    end_date = dt.datetime(2010,1,1)
-    symbols = ['GOOG', 'AAPL', 'GLD', 'XOM', 'IBM']
+    start_date = dt.datetime(2008, 1, 1)
+    end_date = dt.datetime(2009, 12, 31)
+    symbols = ['IBM', 'X', 'HNZ', 'XOM', 'GLD']
 
     # Assess the portfolio
-    allocations, cr, adr, sddr, sr = optimize_portfolio(sd = start_date, ed = end_date,\
-        syms = symbols, \
-        gen_plot = False)
+    allocations, cr, adr, sddr, sr = optimize_portfolio(sd=start_date,
+                                                        ed=end_date,
+                                                        syms=symbols,
+                                                        gen_plot=True)
 
     # Print statistics
     print "Start Date:", start_date
@@ -60,6 +100,7 @@ def test_code():
     print "Volatility (stdev of daily returns):", sddr
     print "Average Daily Return:", adr
     print "Cumulative Return:", cr
+
 
 if __name__ == "__main__":
     # This code WILL NOT be called by the auto grader
--- mc3_p1/LinRegLearner.py
+++ mc3_p1/LinRegLearner.py
@@ -4,12 +4,13 @@ A simple wrapper for linear regression.  (c) 2015 Tucker Balch
 
 import numpy as np
 
+
 class LinRegLearner(object):
 
     def __init__(self, verbose = False):
-        pass # move along, these aren't the drones you're looking for
+        pass  # move along, these aren't the drones you're looking for
 
-    def addEvidence(self,dataX,dataY):
+    def addEvidence(self, dataX, dataY):
         """
         @summary: Add training data to learner
         @param dataX: X values of data to add
@@ -26,10 +27,12 @@ class LinRegLearner(object):
     def query(self,points):
         """
         @summary: Estimate a set of test points given the model we built.
-        @param points: should be a numpy array with each row corresponding to a specific query.
+        @param points: should be a numpy array with each row corresponding to a
+        specific query.
         @returns the estimated values according to the saved model.
         """
-        return (self.model_coefs[:-1] * points).sum(axis = 1) + self.model_coefs[-1]
+        return \
+            (self.model_coefs[:-1] * points).sum(axis=1) + self.model_coefs[-1]
 
-if __name__=="__main__":
+if __name__ == "__main__":
     print "the secret clue is 'zzyzx'"
