18d7187f4c8f5e615e7960a20420683873ba73e4
Anuj Mehta <anuj.nm@gmail.com>
2016-10-02 10:38:54 -0700

    Updating RTLearner

--- mc3_p1/testRTLearner.py
+++ mc3_p1/testRTLearner.py
@@ -4,7 +4,7 @@ Test a learner.  (c) 2015 Tucker Balch
 
 import numpy as np
 import math
-import LinRegLearner as lrl
+import RTLearner as rtl
 import sys
 
 if __name__ == "__main__":
@@ -27,23 +27,43 @@ if __name__ == "__main__":
     print testX.shape
     print testY.shape
 
+    # Overriding trainX:
+    #trainX = np.array(
+    #    [[0.885, 0.33, 9.1, 4.],
+    #     [0.725, 0.39, 10.9, 5.],
+    #     [0.56, 0.5, 9.4, 6.],
+    #     [0.735, 0.57, 9.8, 5.],
+    #     [0.61, 0.63, 8.4, 3.],
+    #     [0.26, 0.63, 11.8, 8.],
+    #     [0.5, 0.68, 10.5, 7.],
+    #     [0.32, 0.78, 10., 6.]
+    #     ])
+
+
     # create a learner and train it
-    learner = lrl.LinRegLearner(verbose=True)  # create a LinRegLearner
+    learner = rtl.RTLearner(1, verbose=True)  # create a LinRegLearner
     learner.addEvidence(trainX, trainY)  # train it
+    print learner.tree
+    #print learner.query([
+    #    [0.7, 0.6, 9.0, 4.0],
+    #    [0.4, 0.7, 10.0, 6.0],
+    #    [0.6, 0.35, 8.5, 3.0]
+    #])
+    #print 'anuj was here'
 
     # evaluate in sample
     predY = learner.query(trainX)  # get the predictions
     rmse = math.sqrt(((trainY - predY) ** 2).sum()/trainY.shape[0])
-    print
+    #print
     print "In sample results"
     print "RMSE: ", rmse
     c = np.corrcoef(predY, y=trainY)
     print "corr: ", c[0, 1]
-
-    # evaluate out of sample
+    #
+    ## evaluate out of sample
     predY = learner.query(testX)  # get the predictions
     rmse = math.sqrt(((testY - predY) ** 2).sum()/testY.shape[0])
-    print
+    #print
     print "Out of sample results"
     print "RMSE: ", rmse
     c = np.corrcoef(predY, y=testY)
