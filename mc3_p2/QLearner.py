c37281cd9ff39e6496e2d8143398517d6526459c
Anuj Mehta <anuj.nm@gmail.com>
2016-11-19 16:36:37 -0800

    Q-Learning for maze navigation.

--- mc3_p2/QLearner.py
+++ mc3_p2/QLearner.py
@@ -5,22 +5,30 @@ Template for implementing QLearner  (c) 2015 Tucker Balch
 import numpy as np
 import random as rand
 
+
 class QLearner(object):
 
-    def __init__(self, \
-        num_states=100, \
-        num_actions = 4, \
-        alpha = 0.2, \
-        gamma = 0.9, \
-        rar = 0.5, \
-        radr = 0.99, \
-        dyna = 0, \
-        verbose = False):
+    def __init__(self,
+                 num_states=100,
+                 num_actions=4,
+                 alpha=0.2,
+                 gamma=0.9,
+                 rar=0.5,
+                 radr=0.99,
+                 dyna=0,
+                 verbose=False):
 
         self.verbose = verbose
         self.num_actions = num_actions
+        self.num_states = num_states
+        self.q_table = (2 * np.random.rand(num_states, num_actions)) - 1
         self.s = 0
         self.a = 0
+        self.alpha = alpha
+        self.gamma = gamma
+        self.rar = rar
+        self.radr = radr
+        self.dyna = dyna
 
     def querysetstate(self, s):
         """
@@ -29,20 +37,61 @@ class QLearner(object):
         @returns: The selected action
         """
         self.s = s
-        action = rand.randint(0, self.num_actions-1)
-        if self.verbose: print "s =", s,"a =",action
-        return action
 
-    def query(self,s_prime,r):
+        random_number = rand.random()
+        if random_number < self.rar:
+            return rand.randint(0, self.num_actions)
+
+        max_value = -float("inf")
+        max_index = None
+        for index, value in enumerate(self.q_table[s]):
+            if value > max_value:
+                max_value = value
+                max_index = index
+        return max_index
+
+    def query(self, s_prime, r):
         """
         @summary: Update the Q table and return an action
         @param s_prime: The new state
-        @param r: The ne state
+        @param r: The real valued immediate reward
         @returns: The selected action
         """
-        action = rand.randint(0, self.num_actions-1)
-        if self.verbose: print "s =", s_prime,"a =",action,"r =",r
-        return action
+        # Update the Q table using s, a s_prime, and r
+        max_value = -float("inf")
+        max_index = None
+        for index, value in enumerate(self.q_table[s_prime]):
+            if value > max_value:
+                max_value = value
+                max_index = index
+        self.q_table[self.s][self.a] = \
+            (1 - self.alpha) * self.q_table[self.s][self.a] + \
+            self.alpha * (r + self.gamma * self.q_table[s_prime][max_index])
 
-if __name__=="__main__":
+        self.s = s_prime
+        # Roll dice to decide whether to take a random action
+        # If you do, choose the random action and return it
+        random_number = rand.random()
+        if random_number < self.rar:
+            action = rand.randint(0, self.num_actions)
+            self.a = action
+
+        # Update rar
+        self.rar *= self.radr
+
+        # Determine what action to take
+        # Q_table is the answer- go to the row s_prime.
+        # Look at the q value for each possible action and choose the one
+        # with the highest value and return that action
+
+        max_value = -float("inf")
+        max_index = None
+        for index, value in enumerate(self.q_table[s_prime]):
+            if value > max_value:
+                max_value = value
+                max_index = index
+        self.a = max_index
+        return max_index
+
+if __name__ == "__main__":
     print "Remember Q from Star Trek? Well, this isn't him"
--- mc3_p2/testqlearner.py
+++ mc3_p2/testqlearner.py
@@ -9,6 +9,7 @@ import time
 import math
 import QLearner as ql
 
+
 # print out the map
 def printmap(data):
     print "--------------------"
@@ -31,6 +32,7 @@ def printmap(data):
         print
     print "--------------------"
 
+
 # find where the robot is in the map
 def getrobotpos(data):
     R = -999
@@ -44,72 +46,76 @@ def getrobotpos(data):
         print "warning: start location not defined"
     return R, C
 
+
 # find where the goal is in the map
 def getgoalpos(data):
     R = -999
     C = -999
     for row in range(0, data.shape[0]):
         for col in range(0, data.shape[1]):
-            if data[row,col] == 3:
+            if data[row, col] == 3:
                 C = col
                 R = row
-    if (R+C)<0:
+    if (R+C) < 0:
         print "warning: goal location not defined"
-    return (R, C)
+    return R, C
+
 
 # move the robot and report reward
-def movebot(data,oldpos,a):
+def movebot(data, oldpos, a):
     testr, testc = oldpos
 
-    randomrate = 0.20 # how often do we move randomly
-    quicksandreward = -100 # penalty for stepping on quicksand
+    randomrate = 0.20  # how often do we move randomly
+    quicksandreward = -100  # penalty for stepping on quicksand
 
     # decide if we're going to ignore the action and 
     # choose a random one instead
     if rand.uniform(0.0, 1.0) <= randomrate: # going rogue
-        a = rand.randint(0,3) # choose the random direction
+        a = rand.randint(0, 3)  # choose the random direction
 
     # update the test location
-    if a == 0: #north
-        testr = testr - 1
-    elif a == 1: #east
-        testc = testc + 1
-    elif a == 2: #south
-        testr = testr + 1
-    elif a == 3: #west
-        testc = testc - 1
+    if a == 0:  # north
+        testr -= 1
+    elif a == 1:  # east
+        testc += 1
+    elif a == 2:  # south
+        testr += 1
+    elif a == 3:  # west
+        testc -= 1
 
-    reward = -1 # default reward is negative one
+    reward = -1  # default reward is negative one
     # see if it is legal. if not, revert
-    if testr < 0: # off the map
+    if testr < 0:  # off the map
         testr, testc = oldpos
-    elif testr >= data.shape[0]: # off the map
+    elif testr >= data.shape[0]:  # off the map
         testr, testc = oldpos
-    elif testc < 0: # off the map
+    elif testc < 0:  # off the map
         testr, testc = oldpos
-    elif testc >= data.shape[1]: # off the map
+    elif testc >= data.shape[1]:  # off the map
         testr, testc = oldpos
-    elif data[testr, testc] == 1: # it is an obstacle
+    elif data[testr, testc] == 1:  # it is an obstacle
         testr, testc = oldpos
-    elif data[testr, testc] == 5: # it is quicksand
+    elif data[testr, testc] == 5:  # it is quicksand
         reward = quicksandreward
-        data[testr, testc] = 6 # mark the event
-    elif data[testr, testc] == 6: # it is still quicksand
+        data[testr, testc] = 6  # mark the event
+    elif data[testr, testc] == 6:  # it is still quicksand
         reward = quicksandreward
-        data[testr, testc] = 6 # mark the event
+        data[testr, testc] = 6  # mark the event
     elif data[testr, testc] == 3:  # it is the goal
-        reward = 1 # for reaching the goal
+        reward = 1  # for reaching the goal
 
-    return (testr, testc), reward #return the new, legal location
+    return (testr, testc), reward  # return the new, legal location
+
 
 # convert the location to a single integer
 def discretize(pos):
     return pos[0]*10 + pos[1]
 
+
 def test(map, iterations, learner, verbose):
 # each iteration involves one trip to the goal
-    startpos = getrobotpos(map) #find where the robot starts
-    goalpos = getgoalpos(map) #find where the goal is
+    startpos = getrobotpos(map)  # find where the robot starts
+    goalpos = getgoalpos(map)  # find where the goal is
     for iteration in range(1,iterations+1): 
         total_reward = 0
         data = map.copy()
@@ -122,69 +128,72 @@ def test(map, iterations, learner, verbose):
             #move to new location according to action and then get a new action
             newpos, stepreward = movebot(data,robopos,action)
             if newpos == goalpos:
-                r = 1 # reward for reaching the goal
+                r = 1  # reward for reaching the goal
             else:
-                r = stepreward # negative reward for not being at the goal
+                r = stepreward  # negative reward for not being at the goal
             state = discretize(newpos)
             action = learner.query(state,r)
     
             if data[robopos] != 6:
-                data[robopos] = 4 # mark where we've been for map printing
+                data[robopos] = 4  # mark where we've been for map printing
             if data[newpos] != 6:
                 data[newpos] = 2 # move to new location
             robopos = newpos # update the location
             #if verbose: time.sleep(1)
             total_reward += stepreward
-            count = count + 1
+            count += 1
         if count == 100000:
             print "timeout"
-        if verbose: printmap(data)
-        if verbose: print iteration, total_reward
+        if verbose:
+            printmap(data)
+        if verbose:
+            print iteration, total_reward
     return total_reward
+
 
 # run the code to test a learner
 def test_code():
 
-    verbose = True # print lots of debug stuff if True
+    verbose = True  # print lots of debug stuff if True
 
     # read in the map
     filename = 'testworlds/world13.csv'
     inf = open(filename)
     data = np.array([map(float,s.strip().split(',')) for s in inf.readlines()])
-    originalmap = data.copy() #make a copy so we can revert to the original map later
+    originalmap = data.copy()  # make a copy so we can revert to the original map later
 
     if verbose: printmap(data)
 
     rand.seed(5)
 
     ######## run non-dyna test ########
-    learner = ql.QLearner(num_states=100,\
-        num_actions = 4, \
-        alpha = 0.2, \
-        gamma = 0.9, \
-        rar = 0.98, \
-        radr = 0.999, \
-        dyna = 0, \
-        verbose=False) #initialize the learner
+    learner = ql.QLearner(num_states=100,
+        num_actions=4,
+        alpha=0.2,
+        gamma=0.9,
+        rar=0.98,
+        radr=0.999,
+        dyna=0,
+        verbose=False)  # initialize the learner
     iterations = 500
     total_reward = test(data, iterations, learner, verbose)
     print "results for", filename
     print iterations, "iterations with total_reward" , total_reward
 
     ######## run dyna test ########
-    learner = ql.QLearner(num_states=100,\
-        num_actions = 4, \
-        alpha = 0.2, \
-        gamma = 0.9, \
-        rar = 0.5, \
-        radr = 0.99, \
-        dyna = 200, \
-        verbose=False) #initialize the learner
+    learner = ql.QLearner(num_states=100,
+        num_actions=4,
+        alpha=0.2,
+        gamma=0.9,
+        rar=0.5,
+        radr=0.99,
+        dyna=200,
+        verbose=False)  # initialize the learner
     iterations = 50
     data = originalmap.copy()
     total_reward = test(data, iterations, learner, verbose)
     print "results for", filename
     print iterations, "iterations with total_reward" , total_reward
 
-if __name__=="__main__":
+if __name__ == "__main__":
     test_code()
